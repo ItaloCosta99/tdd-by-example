51502bb08467bdc125855af35e92d092886d9e34
Italo Costa <italocostajobs@gmail.com>
2023-07-19 16:23:03 -0300

    Change

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public class Money {
+public class Money implements Expression {
   protected int amount;
   protected String currency;
 
@@ -33,6 +33,15 @@ public class Money {
 
   public Money times(int multiplier) {
     return new Money(amount * multiplier, currency);
+  }
+
+  public Expression plus(Money addend) {
+    return new Sum(this, addend);
+  }
+
+  @Override
+  public Money reduce(String toCurrency) {
+    return this;
   }
 
 }
